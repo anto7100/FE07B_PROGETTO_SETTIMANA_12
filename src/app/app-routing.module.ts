3dfc34eae26cb9a1aa409105a41e0a1c4d1d3fc5
anto7100 <98754779+anto7100@users.noreply.github.com>
2022-03-07 12:31:30 +0100

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+
+const routes: Route[] = [
+  {
+    path: 'favourites',
+    canActivate:[AuthGuard],
+    loadChildren: () =>
+      import('./features/favourites/favourites.module').then((m) => m.UsersModule),
+  },
+  {
+    path: 'movies',
+    canActivate:[AuthGuard],
+    loadChildren: () =>
+      import('./features/movies/movies.module').then(
+        (m) => m.MoviesModule
+      ),
+  },
+/*   {
+    path: '**',
+    redirectTo: '',
+  }, */
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
